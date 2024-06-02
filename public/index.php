7ddd3554c0df7cf4f37a7ea4f2501d22f8da366e
anmv921 <andriy.myakush@gmail.com>
2024-06-02 15:44:03 +0100

    router

--- public/index.php
+++ public/index.php
@@ -1,8 +1,8 @@
 <?php
 
-// phpinfo();
-//ini_set('memory_limit', '255M');
-//echo ini_get('memory_limit');
+echo "<pre>";
+print_r($_SERVER);
+echo "</pre>";
 
 $app = include __DIR__ . '/../src/App/bootstrap.php';
 
